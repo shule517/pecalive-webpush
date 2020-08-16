407a2fab19b83296853d15a0ec222ba8f5f46fa5
shule517 <shule517@gmail.com>
2020-08-16 16:59:32 +0900

    Push通知をクリックした時の挙動を試す

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -79,21 +79,20 @@ messaging.setBackgroundMessageHandler(function(payload) {
   self.onnotificationclick = function(event) {
     console.log('On notification click: ', event.notification.tag);
     event.notification.close();
-    return clients.openWindow('http://peca.live');
 
-    // // This looks to see if the current is already open and
-    // // focuses if it is
-    // event.waitUntil(clients.matchAll({
-    //   type: "window"
-    // }).then(function(clientList) {
-    //   for (var i = 0; i < clientList.length; i++) {
-    //     var client = clientList[i];
-    //     if (client.url == '/' && 'focus' in client)
-    //       return client.focus();
-    //   }
-    //   if (clients.openWindow)
-    //     return clients.openWindow('/');
-    // }));
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(clients.matchAll({
+      type: "window"
+    }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+      }
+      if (clients.openWindow)
+        return clients.openWindow('http://peca.live');
+    }));
   };
 
   return self.registration.showNotification(notificationTitle, notificationOptions);

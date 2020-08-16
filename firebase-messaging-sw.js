6861fb2629e6a1c5aa4a286333373b9feafcae61
shule517 <shule517@gmail.com>
2020-08-16 16:44:45 +0900

    Push通知をクリックした時の挙動を追加

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -63,11 +63,17 @@ messaging.setBackgroundMessageHandler(function(payload) {
   const notificationTitle = payload.data.title;
   const notificationOptions = {
     body: payload.data.body,
-    icon: '/pecalive.png', // Push通知メッセージのアイコン
-    badge: '/favicon.png', // スマホヘッダーのバッジ
+    icon: payload.data.icon, // Push通知メッセージのアイコン
+    badge: payload.data.badge, // スマホヘッダーのバッジ
     vibrate: [300, 10, 100, 10, 100],
     requireInteraction: true // タップするまで通知をずっと表示
   };
+
+  // クリックしたら、URLに遷移
+  self.registration.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(self.clients.openWindow(payload.data.url));
+  });
 
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });

0fd8cc36b0a0b255dd89bbc106ac2521e682fd7b
shule517 <shule517@gmail.com>
2020-08-16 17:25:33 +0900

    Push通知をクリックした時の挙動を試す

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -52,24 +52,6 @@ const messaging = firebase.messaging();
  // [END initialize_firebase_in_sw]
  **/
 
-self.onnotificationclick = function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  event.notification.close();
-
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url == '/' && 'focus' in client)
-        return client.focus();
-    }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
-};
 
 // self.addEventListener('onnotificationclick', function(event) {
 //   event.notification.close();
@@ -125,6 +107,25 @@ messaging.setBackgroundMessageHandler(function(payload) {
   //       return clients.openWindow('http://peca.live');
   //   }));
   // };
+
+  self.onnotificationclick = function(event) {
+    console.log('On notification click: ', event.notification.tag);
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(clients.matchAll({
+      type: "window"
+    }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+      }
+      if (clients.openWindow)
+        return clients.openWindow('/');
+    }));
+  };
 
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });
